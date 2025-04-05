37c944a1cdbf8c35c8d6694ef5d488f27bfbbc57
Lanze1507 <ccastellanosp2@miumg.edu.gt>
2025-04-05 10:37:52 -0600

    Septima funcion

---
+++ funcion7.cpp
@@ -0,0 +1,11 @@
+//
+// Created by crist on 5/04/2025.
+//
+
+#include "funcion7.h"
+int main() {
+    system("chcp 65001");
+    cargarDesdeArchivo();
+    menu();
+    return 0;
+}
---
+++ funcion7.h
@@ -0,0 +1,16 @@
+//
+// Created by crist on 5/04/2025.
+//
+
+#ifndef FUNCION7_H
+#define FUNCION7_H
+
+
+
+class funcion7 {
+
+};
+
+
+
+#endif //FUNCION7_H
