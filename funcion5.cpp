11b18b43ad048195ffca3704504663c24612c60f
Lanze1507 <ccastellanosp2@miumg.edu.gt>
2025-04-05 10:35:02 -0600

    Quinta funcion

---
+++ funcion5.cpp
@@ -0,0 +1,9 @@
+//
+// Created by crist on 5/04/2025.
+//
+
+#include "funcion5.h"
+void buscarProducto() {
+    string codigoBuscado;
+    cout << "Ingrese el codigo del producto: ";
+    cin >> codigoBuscado;
---
+++ funcion5.h
@@ -0,0 +1,16 @@
+//
+// Created by crist on 5/04/2025.
+//
+
+#ifndef FUNCION5_H
+#define FUNCION5_H
+
+
+
+class funcion5 {
+
+};
+
+
+
+#endif //FUNCION5_H
